c245845c9b29c73b6b5735e6ae04663e0c73c358
MostafaH04 <mahussein04@gmail.com>
2025-01-01 11:25:26 -0500

    fixed existing problems in codebase

--- tests/TestTaskInterface.py
+++ tests/TestTaskInterface.py
@@ -5,8 +5,9 @@ import os
 
 from TaskInterface.TaskInterface import TaskInterface
 
+
 class TestInterface(unittest.TestCase):
-    def test_add(self):
+    def test_add(self) -> None:
         """
         Test adding new tasks
         """
@@ -15,7 +16,7 @@ class TestInterface(unittest.TestCase):
 
         f = open(file_name + ".json", "w")
         json.dump(file_content, f)
-        f.close()        
+        f.close()
 
         test_add_interface = TaskInterface(file_name)
 
@@ -23,7 +24,7 @@ class TestInterface(unittest.TestCase):
         test_add_interface.add_task("task_1")
         test_add_interface.add_task("task_2")
         test_add_interface.add_task("task_3")
-        
+
         f = open(file_name + ".json", "r")
         content = json.load(f)
         f.close()
@@ -34,26 +35,41 @@ class TestInterface(unittest.TestCase):
         self.assertIn("1", content)
         self.assertIn("2", content)
         self.assertIn("3", content)
-    
+
         self.assertTrue(content["1"]["desc"] == "task_1")
         self.assertTrue(content["2"]["desc"] == "task_2")
         self.assertTrue(content["3"]["desc"] == "task_3")
 
-    def test_delete(self):
+    def test_delete(self) -> None:
         """
         Test deleting exisitng tasks
         """
         file_name = "test_delete_file"
         file_content = {
-            "1":{"desc": "task_1", "status": "TODO", "createdAt": "Wed Jan  1 08:49:30 2025", "updatedAt": "Wed Jan  1 08:49:30 2025"}, 
-            "2": {"desc": "task_2", "status": "TODO", "createdAt": "Wed Jan  1 08:49:30 2025", "updatedAt": "Wed Jan  1 08:49:30 2025"},
-            "3": {"desc": "task_3", "status": "TODO", "createdAt": "Wed Jan  1 08:49:30 2025", "updatedAt": "Wed Jan  1 08:49:30 2025"},
-            "last_id": 3
-            }
+            "1": {
+                "desc": "task_1",
+                "status": "TODO",
+                "createdAt": "Wed Jan  1 08:49:30 2025",
+                "updatedAt": "Wed Jan  1 08:49:30 2025",
+            },
+            "2": {
+                "desc": "task_2",
+                "status": "TODO",
+                "createdAt": "Wed Jan  1 08:49:30 2025",
+                "updatedAt": "Wed Jan  1 08:49:30 2025",
+            },
+            "3": {
+                "desc": "task_3",
+                "status": "TODO",
+                "createdAt": "Wed Jan  1 08:49:30 2025",
+                "updatedAt": "Wed Jan  1 08:49:30 2025",
+            },
+            "last_id": 3,
+        }
 
         f = open(file_name + ".json", "w")
         json.dump(file_content, f)
-        f.close()        
+        f.close()
 
         test_delete_interface = TaskInterface(file_name)
 
@@ -62,7 +78,7 @@ class TestInterface(unittest.TestCase):
         self.assertTrue(test_delete_interface.delete_task(3))
 
         self.assertFalse(test_delete_interface.delete_task(5))
-        
+
         f = open(file_name + ".json", "r")
         content = json.load(f)
         f.close()
@@ -73,24 +89,39 @@ class TestInterface(unittest.TestCase):
         self.assertNotIn("1", content)
         self.assertIn("2", content)
         self.assertNotIn("3", content)
-    
+
         self.assertTrue(content["2"]["desc"] == "task_2")
 
-    def test_update(self):
+    def test_update(self) -> None:
         """
         Test updating exisitng tasks
         """
         file_name = "test_update_file"
         file_content = {
-            "1":{"desc": "task_1", "status": "TODO", "createdAt": "Wed Jan  1 08:49:30 2025", "updatedAt": "Wed Jan  1 08:49:30 2025"}, 
-            "2": {"desc": "task_2", "status": "TODO", "createdAt": "Wed Jan  1 08:49:30 2025", "updatedAt": "Wed Jan  1 08:49:30 2025"},
-            "3": {"desc": "task_3", "status": "TODO", "createdAt": "Wed Jan  1 08:49:30 2025", "updatedAt": "Wed Jan  1 08:49:30 2025"},
-            "last_id": 3
-            }
+            "1": {
+                "desc": "task_1",
+                "status": "TODO",
+                "createdAt": "Wed Jan  1 08:49:30 2025",
+                "updatedAt": "Wed Jan  1 08:49:30 2025",
+            },
+            "2": {
+                "desc": "task_2",
+                "status": "TODO",
+                "createdAt": "Wed Jan  1 08:49:30 2025",
+                "updatedAt": "Wed Jan  1 08:49:30 2025",
+            },
+            "3": {
+                "desc": "task_3",
+                "status": "TODO",
+                "createdAt": "Wed Jan  1 08:49:30 2025",
+                "updatedAt": "Wed Jan  1 08:49:30 2025",
+            },
+            "last_id": 3,
+        }
 
         f = open(file_name + ".json", "w")
         json.dump(file_content, f)
-        f.close()        
+        f.close()
 
         test_update_interface = TaskInterface(file_name)
 
@@ -99,7 +130,7 @@ class TestInterface(unittest.TestCase):
         self.assertTrue(test_update_interface.update_task(2, "updated_task_2"))
 
         self.assertFalse(test_update_interface.update_task(5, "updated_task_5"))
-        
+
         f = open(file_name + ".json", "r")
         content = json.load(f)
         f.close()
@@ -110,26 +141,41 @@ class TestInterface(unittest.TestCase):
         self.assertIn("1", content)
         self.assertIn("2", content)
         self.assertIn("3", content)
-    
+
         self.assertTrue(content["1"]["desc"] == "updated_task_1")
         self.assertTrue(content["2"]["desc"] == "updated_task_2")
         self.assertTrue(content["3"]["desc"] == "task_3")
-    
-    def test_update_progress(self):
+
+    def test_update_progress(self) -> None:
         """
         Test updating progress on exisitng tasks
         """
         file_name = "test_progress_update_file"
         file_content = {
-            "1":{"desc": "task_1", "status": "TODO", "createdAt": "Wed Jan  1 08:49:30 2025", "updatedAt": "Wed Jan  1 08:49:30 2025"}, 
-            "2": {"desc": "task_2", "status": "TODO", "createdAt": "Wed Jan  1 08:49:30 2025", "updatedAt": "Wed Jan  1 08:49:30 2025"},
-            "3": {"desc": "task_3", "status": "TODO", "createdAt": "Wed Jan  1 08:49:30 2025", "updatedAt": "Wed Jan  1 08:49:30 2025"},
-            "last_id": 3
-            }
+            "1": {
+                "desc": "task_1",
+                "status": "TODO",
+                "createdAt": "Wed Jan  1 08:49:30 2025",
+                "updatedAt": "Wed Jan  1 08:49:30 2025",
+            },
+            "2": {
+                "desc": "task_2",
+                "status": "TODO",
+                "createdAt": "Wed Jan  1 08:49:30 2025",
+                "updatedAt": "Wed Jan  1 08:49:30 2025",
+            },
+            "3": {
+                "desc": "task_3",
+                "status": "TODO",
+                "createdAt": "Wed Jan  1 08:49:30 2025",
+                "updatedAt": "Wed Jan  1 08:49:30 2025",
+            },
+            "last_id": 3,
+        }
 
         f = open(file_name + ".json", "w")
         json.dump(file_content, f)
-        f.close()        
+        f.close()
 
         test_progress_interface = TaskInterface(file_name)
 
@@ -138,10 +184,10 @@ class TestInterface(unittest.TestCase):
         self.assertTrue(test_progress_interface.update_task_progress(2, "Done"))
         self.assertTrue(test_progress_interface.update_task_progress(1, "TODO"))
         self.assertTrue(test_progress_interface.update_task_progress(3, "In Progress"))
-        
+
         self.assertFalse(test_progress_interface.update_task_progress(1, "in prog"))
         self.assertFalse(test_progress_interface.update_task_progress(5, "Done"))
-        
+
         f = open(file_name + ".json", "r")
         content = json.load(f)
         f.close()
@@ -152,66 +198,84 @@ class TestInterface(unittest.TestCase):
         self.assertIn("1", content)
         self.assertIn("2", content)
         self.assertIn("3", content)
-    
+
         self.assertTrue(content["1"]["status"] == "TODO")
         self.assertTrue(content["2"]["status"] == "Done")
         self.assertTrue(content["3"]["status"] == "In Progress")
 
-    @patch('builtins.print')
-    def test_listing_tasks_none_empty(self, mock_print):
+    @patch("builtins.print")
+    def test_listing_tasks_none_empty(self, mock_print: unittest.mock.Mock) -> None:
         """
         Test listing exisitng tasks, when task list is populated
         """
         file_name = "test_listing_file"
         file_content = {
-            "1":{"desc": "task_1", "status": "TODO", "createdAt": "Wed Jan  1 08:49:30 2025", "updatedAt": "Wed Jan  1 08:49:30 2025"}, 
-            "last_id": 3
-            }
+            "1": {
+                "desc": "task_1",
+                "status": "TODO",
+                "createdAt": "Wed Jan  1 08:49:30 2025",
+                "updatedAt": "Wed Jan  1 08:49:30 2025",
+            },
+            "last_id": 3,
+        }
 
         f = open(file_name + ".json", "w")
         json.dump(file_content, f)
-        f.close()        
+        f.close()
 
         test_list_interface = TaskInterface(file_name)
 
         test_list_interface.list_tasks()
-        self.assertEqual(mock_print.mock_calls, [
-            call(f'''
+        self.assertEqual(
+            mock_print.mock_calls,
+            [
+                call("""
 ID: 1\n
 Description: task_1\n
 Status: TODO\n
 Creation Time: Wed Jan  1 08:49:30 2025\n
-Last Updated Time: Wed Jan  1 08:49:30 2025'''),
-            call("_" * 60)])     
+Last Updated Time: Wed Jan  1 08:49:30 2025"""),
+                call("_" * 60),
+            ],
+        )
 
         test_list_interface.list_tasks("In Progress")
-        self.assertEqual(mock_print.mock_calls, [
-            call(f'''
+        self.assertEqual(
+            mock_print.mock_calls,
+            [
+                call("""
 ID: 1\n
 Description: task_1\n
 Status: TODO\n
 Creation Time: Wed Jan  1 08:49:30 2025\n
-Last Updated Time: Wed Jan  1 08:49:30 2025'''),
-            call("_" * 60)])   
+Last Updated Time: Wed Jan  1 08:49:30 2025"""),
+                call("_" * 60),
+            ],
+        )
 
         test_list_interface.list_tasks("TODO")
-        self.assertEqual(mock_print.mock_calls, [
-            call(f'''
+        self.assertEqual(
+            mock_print.mock_calls,
+            [
+                call("""
 ID: 1\n
 Description: task_1\n
 Status: TODO\n
 Creation Time: Wed Jan  1 08:49:30 2025\n
-Last Updated Time: Wed Jan  1 08:49:30 2025'''),
-            call("_" * 60),
-            call(f'''
+Last Updated Time: Wed Jan  1 08:49:30 2025"""),
+                call("_" * 60),
+                call("""
 ID: 1\n
 Description: task_1\n
 Status: TODO\n
 Creation Time: Wed Jan  1 08:49:30 2025\n
-Last Updated Time: Wed Jan  1 08:49:30 2025'''),
-            call("_" * 60)])  
+Last Updated Time: Wed Jan  1 08:49:30 2025"""),
+                call("_" * 60),
+            ],
+        )
 
-        os.remove(file_name + ".json")   
+        os.remove(file_name + ".json")
+
 
 if __name__ == "__main__":
     unittest.main()
