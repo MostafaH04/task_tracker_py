80bcd49513e7edff4447755a9cc89637830969e9
Mostafa H <Mahussein04@gmail.com>
2025-01-01 15:27:36 -0500

    Added TaskInterface Docstrings

--- TaskInterface/TaskInterface.py
+++ TaskInterface/TaskInterface.py
@@ -4,11 +4,38 @@ from typing import Union
 
 
 class TaskInterface:
+    """
+    Task Planner Interface Class.
+
+    This class handles updating, storing and retrieving tasks,
+    for the task tracker using a JSON file.
+
+    Attributes:
+        task_file_name (str): JSON File storing tasks
+        content (dict): Content of the JSON File (tasks)
+    """
+
     def __init__(self, fileName: str = "task_file") -> None:
+        """
+        Initializes the task interface.
+
+        This loads up the stored tasks in the JSON file, and
+        manipulates the file based on the function calls.
+
+        Parameters:
+            fileName (str): Name of the JSON file (.json omitted).
+                Defaults to `task_file`.
+        """
         self.task_file_name = fileName + ".json"
         self.content = self._load_file_()
 
     def add_task(self, task_desc: str) -> None:
+        """
+        Adds a new task to the list.
+
+        Parameters:
+            task_desc (str): Description (or name) of the new task.
+        """
         task_id = self._get_last_id_()
         curr_time = datetime.datetime.now()
         time_formated = curr_time.strftime("%c")
@@ -25,12 +52,47 @@ class TaskInterface:
         self._add_content_(new_task)
 
     def delete_task(self, task_id: int) -> bool:
+        """
+        Removes a pre-existing task.
+
+        Fails to remove task if it doesn't exist.
+
+        Parameters:
+            task_id (int): ID corresponding to the task.
+
+        Returns:
+            bool: A boolean representing if the task has been deleted successfully.
+        """
         return self._remove_content_(task_id)
 
     def update_task(self, task_id: int, new_desc: str) -> bool:
+        """
+        Updates a pre-existing task.
+
+        Fails to update task if it doesn't exist.
+
+        Parameters:
+            task_id (int): ID corresponding to the task.
+            new_desc (str): New description for the task.
+
+        Returns:
+            bool: A boolean representing if the task has been updated successfully.
+        """
         return self._update_content_(task_id, new_desc)
 
     def update_task_progress(self, task_id: int, progress: str) -> bool:
+        """
+        Updates the progress on a pre-existing task.
+
+        Fails to update task if it doesn't exist, or
+        if the new status is invalid.
+
+        Parameters:
+            task_id (int): ID corresponding to the task.
+
+        Returns:
+            bool: A boolean representing if the task has been updated successfully.
+        """
         if progress != "TODO" and progress != "In Progress" and progress != "Done":
             return False
 
